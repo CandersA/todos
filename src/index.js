ab9ca2ac6d0bae14843e8a19cb9875518eceb4d6
CandersA <123170339+CandersA@users.noreply.github.com>
2023-02-14 20:24:41 +0200

    Add functions to create a project and add a todo to a specific project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import './styles.css';
 
 const element = document.getElementById('content');
+const addProj = document.querySelector('.create-proj');
+const toDoProj = document.querySelector('.add-todo');
+const showProject = document.querySelector('.showoff');
 element.textContent = 'Hello webpack';
+
+const getChecklist = (() => {
+
+})();
+
+const getNotes = (() => {
+
+})();
 
 const toDo = (title, description, dueDate) => {
   const complete = false;
   const checkList = () => getChecklist();
+  const priority = 0;
 
+  const notes = () => getNotes();
   const getTitle = () => title;
   const getDesc = () => description;
   const getDueDate = () => dueDate;
@@ -14,6 +27,82 @@ const toDo = (title, description, dueDate) => {
   return { getTitle, getDesc, getDueDate };
 };
 
-const todo1 = toDo('Make bed', 'I want to make the bed', 'Tomorrow');
+const Project = (title) => {
+  const todos = [];
+  const getTitle = () => title;
 
-console.log(todo1);
+  function addToDo(id, todoTitle, description, dueDate) {
+    const newToDo = toDo(todoTitle, description, dueDate);
+    todos.push(newToDo);
+  }
+
+  return { getTitle, addToDo, todos };
+};
+
+const allProjects = (() => {
+  const projects = [];
+
+  function addProject(projectName) {
+    projects.push(Project(projectName));
+  }
+
+  return { addProject, projects };
+})();
+
+const displayController = (() => {
+  const { projects } = allProjects;
+
+  function displayProjects() {
+    let i = 0;
+    let projectAddTodo;
+
+    element.textContent = '';
+    projects.forEach((project) => {
+      const projectDiv = document.createElement('div');
+      const projectSpan = document.createElement('span');
+      projectAddTodo = document.createElement('button');
+      projectAddTodo.textContent = 'Add ToDo to this project';
+      projectAddTodo.setAttribute('id', i);
+      projectAddTodo.classList.add('project-add');
+      projectDiv.classList.add('project');
+      projectSpan.textContent = project.getTitle();
+      projectDiv.appendChild(projectSpan);
+      projectDiv.appendChild(projectAddTodo);
+      element.appendChild(projectDiv);
+      i += 1;
+      return { i, projectAddTodo };
+    });
+  }
+
+  return { displayProjects };
+})();
+
+addProj.addEventListener('click', () => {
+  const projName = prompt('Please enter project name');
+  allProjects.addProject(projName);
+  displayController.displayProjects();
+  const projectsDOM = Array.from(document.getElementsByClassName('project-add'));
+
+  projectsDOM.forEach((projectDOM) => {
+    projectDOM.addEventListener('click', () => {
+      const todoName = prompt('Please enter name');
+      const todoDesc = prompt('Please enter description');
+      const todoDuoDate = prompt('Please enter due date');
+      allProjects.projects[projectDOM.id].addToDo(todoName, todoDesc, todoDuoDate);
+    });
+  });
+
+  console.log(allProjects.projects);
+});
+
+toDoProj.addEventListener('click', () => {});
+showProject.addEventListener('click', () => {});
+
+// Click on add project
+
+/// / Add a project to website with (Input title)
+
+// Click on add todo to specific project
+
+/// / (Input Title, Description, DueDate, priority)
+/// / Get project that has been clicked and add toDo to that project
