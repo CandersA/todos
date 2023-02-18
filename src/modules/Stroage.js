135d82381950421784db0c10d9b4bb3889c645fa
CandersA <123170339+CandersA@users.noreply.github.com>
2023-02-18 20:58:22 +0200

    A lot has changed, forgot to commit again!

--- src/modules/Stroage.js
+++ src/modules/Stroage.js
@@ -1,7 +1,6 @@
 import TodoList from "./TodoList";
 
 const Storage = (() => {
-
   const changeProjectName = (project, newName) => {
     project.setName(newName);
   };
@@ -15,11 +14,33 @@ const Storage = (() => {
   };
 
   const getProjects = () => {
-    const ProjectNames = TodoList.projects.map((project) => project.getName());
+    const ProjectNames = TodoList.projects.map((project) =>
+      project.getName()
+    );
     return ProjectNames;
   };
 
-  return { changeProjectName, changeTaskDate, changeTaskName, getProjects };
+  const taskDisplay = (taskName, taskDate) => {
+    const taskHTML = `<div class="task">
+      <span>${taskName}</span>
+      <span>${taskDate}</span>
+      <div class="taskicons">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>Edit Task</title><path d="M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9 16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z" /></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>Change priority</title><path d="M14.4,6L14,4H5V21H7V14H12.6L13,16H20V6H14.4Z" /></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>Move to different project</title><path d="M14,18V15H10V11H14V8L19,13M20,6H12L10,4H4C2.89,4 2,4.89 2,6V18A2,2 0 0,0 4,20H20A2,2 0 0,0 22,18V8C22,6.89 21.1,6 20,6Z" /></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>Delete</title><path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z" /></svg>
+      </div>
+    </div>`;
+    return taskHTML;
+  };
+
+  return {
+    changeProjectName,
+    changeTaskDate,
+    changeTaskName,
+    getProjects,
+    taskDisplay,
+  };
 })();
 
 export default Storage;
--- src/modules/UI.js
+++ src/modules/UI.js
@@ -16,25 +16,49 @@ const UI = (() => {
     projectTasks.forEach((task) => {
       const taskName = task.getName();
       const taskDate = task.getDate();
-      taskDiv.innerHTML += `<div class="task">
-        <span>${taskName}</span>
-        <span>${taskDate}</span>
-        <div class="taskicons">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>Edit Task</title><path d="M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9 16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z" /></svg>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>Change priority</title><path d="M14.4,6L14,4H5V21H7V14H12.6L13,16H20V6H14.4Z" /></svg>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>Move to different project</title><path d="M14,18V15H10V11H14V8L19,13M20,6H12L10,4H4C2.89,4 2,4.89 2,6V18A2,2 0 0,0 4,20H20A2,2 0 0,0 22,18V8C22,6.89 21.1,6 20,6Z" /></svg>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>Delete</title><path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z" /></svg>
-        </div>
-      </div>`;
+      taskDiv.innerHTML += Storage.taskDisplay(
+        taskName,
+        taskDate
+      );
     });
+  };
+
+  const getProjectHeading = (project) => {
+    const headingDiv =
+    document.querySelector(".project-head");
+    headingDiv.textContent = "";
+    const projectHeading = project.getName();
+    headingDiv.prepend(projectHeading);
+  };
+
+  const changeStateofButton = (newState) => {
+    const addTodo = document.querySelector(".addicon");
+
+    if (newState === "close") {
+      addTodo.innerHTML = "Close";
+      addTodo.style.width = "70px";
+      addTodo.style.right = "-100px";
+    } else if (newState === "open") {
+      addTodo.innerHTML =
+        "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' height='50px'><title>Add task</title><path d='M17,13H13V17H11V13H7V11H11V7H13V11H17M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z' /></svg>";
+      addTodo.style.width = "50px";
+      addTodo.style.right = "-80px";
+    }
   };
 
   const togglePopup = () => {
     const popUp = document.querySelector(".addpopup");
+    const disableElements =
+      document.querySelector(".disable");
+
     if (popUp.classList.contains("active")) {
       popUp.classList.remove("active");
+      disableElements.classList.remove("active");
+      changeStateofButton("open");
     } else {
       popUp.classList.add("active");
+      disableElements.classList.add("active");
+      changeStateofButton("close");
     }
   };
 
@@ -44,17 +68,23 @@ const UI = (() => {
     const taskProject =
       document.getElementById("assignproject");
     let selectedProject = "";
+
     popUp.addEventListener("keydown", (e) => {
       if (
         e.key === "Enter" &&
-        popUp.classList.contains("active")
+        popUp.classList.contains("active") &&
+        selectedProject.value !== ""
       ) {
         selectedProject = TodoList.getProject(
           taskProject.value
         );
-        selectedProject.addTask(
-          Task(taskName.value, taskDate.value)
-        );
+        if (taskDate.value === "") {
+          selectedProject.addTask(Task(taskName.value));
+        } else {
+          selectedProject.addTask(
+            Task(taskName.value, taskDate.value)
+          );
+        }
         displayTasks(selectedProject);
         togglePopup();
       }
@@ -65,6 +95,16 @@ const UI = (() => {
     if (e.key === "Escape") {
       togglePopup();
     }
+  };
+
+  const addProjectToSelection = (id, projectName) => {
+    const projectSelect =
+      document.getElementById("assignproject");
+    const newSelection = document.createElement("option");
+
+    newSelection.setAttribute("value", id);
+    newSelection.textContent = projectName;
+    projectSelect.appendChild(newSelection);
   };
 
   const displayProject = (project, id) => {
@@ -78,14 +118,11 @@ const UI = (() => {
     projectDiv.appendChild(projectSpan);
     appendToDiv.appendChild(projectDiv);
 
-    const projectSelect = document.getElementById("assignproject");
-    const newSelection = document.createElement("option");
-    newSelection.setAttribute("value", id);
-    newSelection.textContent = projectName;
-    projectSelect.appendChild(newSelection);
+    addProjectToSelection(id, projectName);
 
     projectDiv.addEventListener("click", () => {
       displayTasks(TodoList.getProject(id));
+      getProjectHeading(TodoList.getProject(id));
     });
   };
 
@@ -99,6 +136,7 @@ const UI = (() => {
 
     addProjectButton.style.display = "none";
     inputProjectName.style.display = "block";
+    inputProjectName.focus();
     inputProjectName.addEventListener("keydown", (e) => {
       if (e.key === "Enter") {
         const projectName = inputProjectName.value;
@@ -127,9 +165,13 @@ const UI = (() => {
   };
 
   const loadHomepage = () => {
+    const defaultProject = TodoList.getProject(0);
+
     document.addEventListener("keydown", handleKeyInput);
     initEventListeners();
-    displayProject(TodoList.getProject(0), 0);
+    displayProject(defaultProject, 0);
+    displayTasks(defaultProject);
+    getProjectHeading(defaultProject);
     id += 1;
   };
 
